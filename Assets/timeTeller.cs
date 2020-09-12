cbddde8c7a27c656720040f229d5dbb90c73ce09
Zeke <49454258+RunningMonkeys@users.noreply.github.com>
2020-09-12 15:04:22 -0500

    Initial Commit of work from HW!

---
+++ Assets/timeTeller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class timeTeller : MonoBehaviour
+{
+    public GameObject timeTextObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    InvokeRepeating("UpdateTime", 0f, 10f);   
+    }
+
+    // Update is called once per frame
+    void UpdateTime()
+    {
+    timeTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("h:mm tt");
+       
+    }
+}
