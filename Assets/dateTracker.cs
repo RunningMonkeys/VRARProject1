43ce10ccf5e2634d5054d52830f909b29e3b7296
Zeke <49454258+RunningMonkeys@users.noreply.github.com>
2020-09-12 15:34:41 -0500

    Added Date To project

---
+++ Assets/dateTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class dateTracker : MonoBehaviour
+{
+    public GameObject dateTextObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		InvokeRepeating("UpdateDate", 0f, 100f);   
+    }
+
+    // Update is called once per frame
+    void UpdateDate()
+    {
+		dateTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("MM/dd/yy");
+    }
+}
